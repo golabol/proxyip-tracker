fdd261328f84868ec780e0e14bce50f24831e20e
golabol <mireiland@westtelecom.cloud>
2025-04-01 18:09:29 +0330

    Update getIPs.py

--- scripts/getIPs.py
+++ scripts/getIPs.py
@@ -6,6 +6,90 @@ import configparser
 import json
 import re
 
+countries = {
+    "ID": "Indonesia",
+    "SG": "Singapore",
+    "JP": "Japan",
+    "AT": "Austria",
+    "US": "United States",
+    "MY": "Malaysia",
+    "CA": "Canada",
+    "GB": "United Kingdom",
+    "IN": "India",
+    "IR": "Iran",
+    "AE": "United Arab Emirates",
+    "FI": "Finland",
+    "TR": "Turkey",
+    "MD": "Moldova",
+    "TW": "Taiwan",
+    "CH": "Switzerland",
+    "SE": "Sweden",
+    "NL": "Netherlands",
+    "ES": "Spain",
+    "RU": "Russia",
+    "RO": "Romania",
+    "PL": "Poland",
+    "MX": "Mexico",
+    "AL": "Albania",
+    "IT": "Italy",
+    "DE": "Germany",
+    "NZ": "New Zealand",
+    "FR": "France",
+    "AM": "Armenia",
+    "CY": "Cyprus",
+    "DK": "Denmark",
+    "BR": "Brazil",
+    "KR": "South Korea",
+    "VN": "Vietnam",
+    "TH": "Thailand",
+    "HK": "Hong Kong",
+    "CN": "China",
+    "AU": "Australia",
+    "AR": "Argentina",
+    "BE": "Belgium",
+    "BG": "Bulgaria",
+    "AZ": "Azerbaijan",
+    "CO": "Colombia",
+    "CZ": "Czech Republic",
+    "EE": "Estonia",
+    "GI": "Gibraltar",
+    "IE": "Ireland",
+    "HU": "Hungary",
+    "IL": "Israel",
+    "KZ": "Kazakhstan",
+    "LT": "Lithuania",
+    "LU": "Luxembourg",
+    "LV": "Latvia",
+    "PH": "Philippines",
+    "PT": "Portugal",
+    "PR": "Puerto Rico",
+    "QA": "Qatar",
+    "RS": "Serbia",
+    "SC": "Seychelles",
+    "SA": "Saudi Arabia",
+    "SK": "Slovakia",
+    "UA": "Ukraine",
+    "UZ": "Uzbekistan",
+    "T1": "Unknown"
+}
+tls_port = {
+    443,
+    2053,
+    2083,
+    2087,
+    2096,
+    8443
+}
+nontls_port = {
+    80,
+    2052,
+    2082,
+    2086,
+    2095,
+    8080
+}
+added_ips = set()
+
 def download_zip_file(url):
     """Download the ZIP file from the specified URL."""
     try:
@@ -17,12 +101,12 @@ def download_zip_file(url):
 
 def extract_and_combine_files(zip_file, file_pattern):
     """Extract and combine content of files matching the pattern."""
+    global added_ips
     matching_files = fnmatch.filter(zip_file.namelist(), file_pattern)
     if not matching_files:
         raise FileNotFoundError(f"No files matching {file_pattern} found in the ZIP archive.")
 
     combined_content = []
-    added_ips = set()
     for file_name in matching_files:
         pattern = r"(\d+)-([0-1])-(\d+)\.txt"
         match = re.match(pattern, file_name)
@@ -43,15 +127,43 @@ def save_to_file(content, output_file):
         file.write(content)
     print(f"Combined content saved to: {output_file}")
 
+def get_country_based_ips():
+    global added_ips
+    location_ips_data = {}
+    base_url = 'https://cfip.ashrvpn.v6.army/?country='
+    for con in countries:
+        try:
+            response = requests.get(base_url + con)
+            response.raise_for_status()
+            for line in response.text.split('\n'):
+                ip, port = line.split('|').strip().split(':')
+                if ip not in added_ips:
+                    location_ips_data.update({
+                        'ip': ip,
+                        'port': int(port),
+                        'country': con
+                    })
+                    added_ips.add(ip)
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(f"Error during access to {base_url+con}: {e}")
+    return location_ips_data
+
 def process_zip_file(url, file_pattern, output_file):
     """Main function to download, process, and save the combined content."""
     print(f"Downloading ZIP file from: {url}")
     zip_data = download_zip_file(url)
+    country_based_ips_data = get_country_based_ips()
 
     with zipfile.ZipFile(zip_data) as zip_file:
         print("Extracting and combining files...")
         combined_content = extract_and_combine_files(zip_file, file_pattern)
-
+        for item in country_based_ips_data:
+            combined_content.append({
+                'ip': item.get('ip'),
+                'asn': None,
+                'tls': 'Unknown',
+                'port': item.get('port'),
+            })
         print("Saving combined content to file...")
         save_to_file(combined_content, output_file)
 
